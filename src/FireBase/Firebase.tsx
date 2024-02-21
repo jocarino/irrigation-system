d22fabac7d2ea21a60eb08fe5a5cc1e61fddc056
jocarino <joao.goncalo.veloso@gmail.com>
2024-02-21 19:01:55 +0000

    feat: data from firebase and chart

--- src/FireBase/Firebase.tsx
+++ src/FireBase/Firebase.tsx
@@ -1,25 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    // measurementId: process.env.REACT_APP_MEASUREMENTID,
-    databaseURL: process.env.REACT_APP_DATABASE_URL
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  // measurementId: process.env.REACT_APP_MEASUREMENTID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export firestore
+export const db = getFirestore(app);
 
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
--- src/pages/MainDashboard.tsx
+++ src/pages/MainDashboard.tsx
@@ -1,31 +1,84 @@
-import { onValue, ref } from 'firebase/database';
-import React from 'react';
-import { database } from '../FireBase/Firebase';
+import React, { Suspense } from "react";
+import { database, db } from "../FireBase/Firebase";
+import {
+  DocumentData,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
+import { onValue, ref } from "firebase/database";
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
-type PlantData =
-    { moisture: [{ [key: string]: number }], lastMoisture: { date: string, level: number } }
-
+type PlantData = {
+  moisture: [{ [key: string]: number }];
+  lastMoisture: { date: string; level: number };
+};
 
 const MainDashboard: React.FC = () => {
-    const [plantData, setPlantData] = React.useState<{ [key: string]: PlantData }>({})
+  const [plantData, setPlantData] = React.useState<{
+    [key: string]: PlantData;
+  }>({});
+  const [plantHumidityHistory, setPlantHumdityHistory] = React.useState<{
+    [plant: string]: {
+      date: string;
+      value: any;
+    }[];
+  }>({});
 
-    React.useEffect(() => {
-        const starCountRef = ref(database, 'plant')
-        onValue(starCountRef, (snapshot: any) => {
-            const data = snapshot.val();
-            setPlantData(data);
-            console.log(data);
+  const getPlantHumidity = async () => {
+    const querySnapshot = await getDocs(collection(db, "humidity"));
+    const plantHumidity = querySnapshot.docs.reduce((acc: any, cur) => {
+      acc[cur.id] = Object.entries(cur.data()).map(([date, value]) => ({
+        date: date,
+        value: value,
+      }));
+      return acc;
+    }, {});
 
-        }, { onlyOnce: true });
-    }, [])
+    setPlantHumdityHistory(plantHumidity);
+  };
 
-
-    return (
-        <>
-            <p>Test1</p>
-            {Object.keys(plantData).length !== 0 && Object.keys(plantData).map((plant: string) => <p key={plant + '-item'}>{plant}: {plantData?.[plant]?.lastMoisture.level}</p>)}
-        </>
+  React.useEffect(() => {
+    getPlantHumidity();
+    const starCountRef = ref(database, "plant");
+    onValue(
+      starCountRef,
+      (snapshot: any) => {
+        const data = snapshot.val();
+        setPlantData(data);
+      },
+      { onlyOnce: true }
     );
+  }, []);
+
+  return (
+    <div className="p-6">
+      <p className="font-bold">Test1</p>
+      {Object.keys(plantData).length !== 0 &&
+        Object.keys(plantData).map((plant: string) => (
+          <div className="bg-green-600 rounded-md h-36 w-80 p-4 mt-2">
+            <p key={plant + "-item"}>
+              {plant}: {plantData?.[plant]?.lastMoisture.level}
+            </p>
+            <VictoryChart theme={VictoryTheme.material}>
+              <VictoryLine
+                style={{
+                  data: { stroke: "#c43a31" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={plantHumidityHistory[plant]
+                  ?.slice(0, 10)
+                  ?.map((plant) => ({ x: plant.date, y: plant.value }))}
+              />
+            </VictoryChart>
+          </div>
+        ))}
+      <Suspense fallback={"Loading..."}>
+        {JSON.stringify(plantHumidityHistory)}
+      </Suspense>
+    </div>
+  );
 };
 
 export default MainDashboard;
